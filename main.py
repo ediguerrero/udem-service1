987c532d97d050451deb8615f06bc5e5e2436f5f
ediguerrero <edixon_guerrero96@hotmail.com>
2022-08-17 19:34:58 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import Union
+
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+
+@app.get("/infoUsers/{idUsuario}")
+def read_item(idUsuario: str, q: Union[str, None] = None):
+    url = 'https://62fc4666abd610251c17fdae.mockapi.io/api/User/?idUsuario=' + idUsuario
+    response = requests.get(url, {}, timeout=5)
+    return {"items": response.json()}
