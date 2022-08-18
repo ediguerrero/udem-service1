8e259ff977704a54056fd163c54761da5d39f1ac
ediguerrero <edixon_guerrero96@hotmail.com>
2022-08-17 20:18:58 -0500

    add  fix rest

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 
 @app.get("/infoUsers/{idUsuario}")
-def read_item(idUsuario: str, q: Union[str, None] = None):
+def read_item(idUsuario: str):
     url = 'https://62fc4666abd610251c17fdae.mockapi.io/api/User/?idUsuario=' + idUsuario
     response = requests.get(url, {}, timeout=5)
     return {"items": response.json()}
